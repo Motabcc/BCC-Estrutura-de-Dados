bcf63e343dea264c3db068266086804c4839811b
Gabriel Mota <gabrielalbertomota1997@gmail.com>
2026-08-31 11:03:49 -0300

    Imprime e Isere Ultimo elemento

--- Node.java
+++ Node.java
@@ -12,6 +12,13 @@ public class Node {
 class ListaSe{
     public Node primeiro;
     public Node ultimo;
+
+    public boolean vazia(){
+        if(this.primeiro == null){
+            return true;
+        }
+        else{return false;}
+    }
     public void inserePrimeiro(int info){
         //Criando nó e inserindo a info nele
         Node p =new Node(info);
@@ -25,5 +32,22 @@ class ListaSe{
             this.primeiro=p;
         }
     }
+    public void imprimir(){
+        Node p = this.primeiro;
+        while(p != null){
+            System.out.println(p.info);
+            p=p.proximo;
+        }
+    }
 
+    public void insereUltimo(int info){
+        Node p = new Node(info);
+        if(this.vazia()) {
+        this.primeiro=p;
+        }else{
+            this.ultimo.proximo=p;
+        }
+        this.ultimo= p;
+
+    }
 }
