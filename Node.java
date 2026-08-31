90535a786c9e694b687dc31a89b7e9101ba2b16c
Gabriel Mota <gabrielalbertomota1997@gmail.com>
2026-08-31 10:21:02 -0300

    Lista Encadeada com descritor

---
+++ Node.java
@@ -0,0 +1,28 @@
+public class Node {
+    public int info;
+    public Node proximo;
+
+    public Node(int info){
+        Node lista;
+        this.info=info;
+        this.proximo= null;
+    }
+}
+
+class ListaSe{
+    public Node primeiro;
+    public Node ultimo;
+    public void inserePrimeiro(int info){
+        //Criando nó e inserindo a info nele
+        Node p =new Node(info);
+        if(this.vazia()){
+            this.primeiro =p;
+            this.ultimo =p;
+
+        }
+        else{
+
+        }
+    }
+
+}
